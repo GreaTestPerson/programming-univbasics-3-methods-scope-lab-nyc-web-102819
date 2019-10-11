026868a0686adddd5e555af326cf6b99b2ca1378
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-11 03:32:37 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,6 @@
 def mario 
-  phrase ="It's-a me, Mario!\n'"
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase ="It's-a me, Mario!'"
   puts phrase
 end
 
@@ -14,5 +15,24 @@ end
 
 def any_phrase 
   phrase = "Do A Barrel Roll!\n"
+  puts phrase
+end
+
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
+  puts phrase
+end
+
+def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
+end
+
+def link 
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+def any_phrase (phrase)
   puts phrase
 end
